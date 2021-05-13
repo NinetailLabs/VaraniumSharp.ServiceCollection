a57aa026440de6eda2b0e837b7bca21f0c4e615f
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-05-13 15:07:16 +0200

    Initial commit

---
+++ VaraniumSharp.ServiceCollection.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VaraniumSharp.ServiceCollection.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ VaraniumSharp.ServiceCollection/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VaraniumSharp.ServiceCollection
+{
+    public class Class1
+    {
+    }
+}
