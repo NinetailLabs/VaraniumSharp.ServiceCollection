53782c8d6a365a0e70b0723d6f7e90b4cdb13a9a
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-05-14 16:29:34 +0200

    Implementation.
    - Completed ContainerSetup implementation and testing

--- VaraniumSharp.ServiceCollection.Tests/ContainerSetupTests.cs
+++ VaraniumSharp.ServiceCollection.Tests/ContainerSetupTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -10,7 +12,138 @@ namespace VaraniumSharp.ServiceCollection.Tests
     public class ContainerSetupTests
     {
         [Fact]
-        public void TryingToResolveEntryThrowsAnException()
+        public void ClassWithMultipleConstructorsIsRegisteredCorrectly()
+        {
+            // arrange
+            var serviceDummy = new Mock<IServiceCollection>();
+            var sut = new ContainerSetup(serviceDummy.Object);
+            var act = new Action(() => sut.RetrieveClassesRequiringRegistration(true));
+
+            // act
+            // assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Fact]
+        public void ConcretionClassesAreResolvedCorrectly()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveConcretionClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClass = ((IEnumerable<BaseClassDummy>)services.GetService(typeof(IEnumerable<BaseClassDummy>))).ToList();
+            resolvedClass.Count.Should().Be(1);
+            resolvedClass.First().GetType().Should().Be(typeof(InheritorClassDummy));
+        }
+
+        [Fact]
+        public void ConcretionClassesCorrectlyApplyReuse()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveConcretionClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClasses = ((IEnumerable<ITestInterfaceDummy>) services.GetService(typeof(IEnumerable<ITestInterfaceDummy>))).ToList();
+            var secondResolve = ((IEnumerable<ITestInterfaceDummy>)services.GetService(typeof(IEnumerable<ITestInterfaceDummy>))).ToList();
+            resolvedClasses.Should().BeEquivalentTo(secondResolve);
+        }
+
+        [Fact]
+        public void ConcretionClassesFromInterfaceAreCorrectlyResolved()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveConcretionClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClasses = ((IEnumerable<ITestInterfaceDummy>)services.GetService(typeof(IEnumerable<ITestInterfaceDummy>))).ToList();
+            resolvedClasses.Count.Should().Be(2);
+            resolvedClasses.Should().Contain(x => x.GetType() == typeof(ImplementationClassDummy));
+            resolvedClasses.Should().Contain(x => x.GetType() == typeof(ImplementationClassTooDummy));
+        }
+
+        [Fact]
+        public void ConcretionClassWithMultipleConstructorsIsRegisteredCorrectly()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+            var act = new Action(() => sut.RetrieveConcretionClassesRequiringRegistration(true));
+
+            // act
+            // assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Fact]
+        public void MultiTypeRegistrationSingletonsWorkCorrectly()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveConcretionClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClasses = ((IEnumerable<ITestInterfaceDummy>)services.GetService(typeof(IEnumerable<ITestInterfaceDummy>))).ToList();
+            var interfaceResolvedClass = resolvedClasses.FirstOrDefault(t => t.GetType() == typeof(ImplementationClassDummy));
+            var directlyResolvedClass = services.GetService(typeof(ImplementationClassDummy));
+
+            interfaceResolvedClass.Should().Be(directlyResolvedClass);
+        }
+
+        [Fact]
+        public void SingletonRegistrationsAreResolvedCorrectly()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClass = services.GetService<SingletonDummy>();
+            var secondResolve = services.GetService<SingletonDummy>();
+            resolvedClass.Should().Be(secondResolve);
+        }
+
+        [Fact]
+        public void TransientRegistrationsAreResolvedCorrectly()
+        {
+            // arrange
+            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var sut = new ContainerSetup(serviceCollection);
+
+            // act
+            sut.RetrieveClassesRequiringRegistration(true);
+
+            // assert
+            var services = serviceCollection.BuildServiceProvider();
+            var resolvedClass = services.GetService<AutoRegistrationDummy>();
+            var secondResolve = services.GetService<AutoRegistrationDummy>();
+            resolvedClass.Should().NotBe(secondResolve);
+        }
+
+        [Fact]
+        public void TryingToResolveAServiceThrowsANotSupportedException()
         {
             // arrange
             var serviceCollectionDummy = new Mock<IServiceCollection>();
@@ -21,7 +154,22 @@ namespace VaraniumSharp.ServiceCollection.Tests
 
             // act
             // assert
-            act.Should().Throw<NotImplementedException>();
+            act.Should().Throw<NotSupportedException>();
+        }
+
+        [Fact]
+        public void TryingToResolveMultipleServicesThrowsANotSupportedException()
+        {
+            // arrange
+            var serviceCollectionDummy = new Mock<IServiceCollection>();
+
+            var sut = new ContainerSetup(serviceCollectionDummy.Object);
+
+            var act = new Action(() => sut.ResolveMany<DisposableDummy>());
+
+            // act
+            // assert
+            act.Should().Throw<NotSupportedException>();
         }
     }
 }
--- VaraniumSharp.ServiceCollection/ContainerSetup.cs
+++ VaraniumSharp.ServiceCollection/ContainerSetup.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using VaraniumSharp.Attributes;
 using VaraniumSharp.DependencyInjection;
+using VaraniumSharp.Enumerations;
+using VaraniumSharp.ServiceCollection.Extensions;
 
 namespace VaraniumSharp.ServiceCollection
 {
@@ -27,13 +33,13 @@ namespace VaraniumSharp.ServiceCollection
         /// <inheritdoc />
         public override TService Resolve<TService>()
         {
-            throw new System.NotImplementedException();
+            throw new System.NotSupportedException(ResolveErrorMessage);
         }
 
         /// <inheritdoc />
         public override IEnumerable<TService> ResolveMany<TService>()
         {
-            throw new System.NotImplementedException();
+            throw new System.NotSupportedException(ResolveErrorMessage);
         }
 
         #endregion
@@ -43,18 +49,66 @@ namespace VaraniumSharp.ServiceCollection
         /// <inheritdoc />
         protected override void RegisterClasses()
         {
-            throw new System.NotImplementedException();
+            foreach (var @class in ClassesToRegister)
+            {
+                var registrationAttribute =
+                    (AutomaticContainerRegistrationAttribute)
+                    @class.GetCustomAttribute(typeof(AutomaticContainerRegistrationAttribute));
+
+                if (registrationAttribute.Reuse == ServiceReuse.Singleton)
+                {
+                    _services.AddSingleton(registrationAttribute.ServiceType, @class);
+                }
+                else if (registrationAttribute.Reuse == ServiceReuse.Default)
+                {
+                    _services.AddTransient(registrationAttribute.ServiceType, @class);
+                }
+                else
+                {
+                    throw new NotImplementedException($"Reuse type {registrationAttribute.Reuse} is not implemented for this ContainerSetup");
+                }
+            }
         }
 
         /// <inheritdoc />
         protected override void RegisterConcretionClasses()
         {
-            throw new System.NotImplementedException();
+            foreach (var @class in ConcretionClassesToRegister)
+            {
+                var registrationAttribute =
+                    (AutomaticConcretionContainerRegistrationAttribute)
+                    @class.Key.GetCustomAttribute(typeof(AutomaticConcretionContainerRegistrationAttribute));
+
+                @class.Value.ForEach(x =>
+                {
+                    if (registrationAttribute.Reuse == ServiceReuse.Singleton)
+                    {
+                        _services
+                            .AddSingleton(@class.Key, x)
+                            .ReUseSingleton(x, @class.Key);
+                    }
+                    else if (registrationAttribute.Reuse == ServiceReuse.Default)
+                    {
+                        _services.AddTransient(@class.Key, x);
+                        _services.AddTransient(x, x);
+                    }
+                    else
+                    {
+                        throw new NotImplementedException($"Reuse type {registrationAttribute.Reuse} is not implemented for this ContainerSetup");
+                    }
+                });
+
+            }
         }
 
         #endregion
 
         #region Variables
+
+        /// <summary>
+        /// Error message to show when the user tries to resolve an entry from the container
+        /// </summary>
+        private const string ResolveErrorMessage = "IServiceCollection does not support resolving during setup time. To resolve services from the container get the Services instance from the host after configuration is completed";
 
         /// <summary>
         /// ServiceCollection instance
